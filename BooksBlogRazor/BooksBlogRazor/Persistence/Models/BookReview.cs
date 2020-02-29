c3f9a827d0a499632787bb3073f290721b3329cf
Leonid Kurepin <leonidkurepin@yandex.ru>
2020-02-29 20:44:31 +0400

    Create solution and persistence infrastructure

---
+++ BooksBlogRazor/BooksBlogRazor/Persistence/Models/BookReview.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BooksBlogRazor.Persistence.Models
+{
+    public class BookReview
+    {
+        public int BookReviewId { get; set; }
+        public string Title { get; set; }
+        public string Image { get; set; }
+        public string Genre { get; set; }
+        public int? MyRating { get; set; }
+        public string Description { get; set; }
+        public string Review { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime? ReviewDateCreated { get; set; }
+    }
+}
