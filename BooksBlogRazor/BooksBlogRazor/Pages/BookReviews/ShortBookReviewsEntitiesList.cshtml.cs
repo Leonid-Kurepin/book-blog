f28f19fe93a823872f5249f7afe1417ad2f53feb
Leonid Kurepin <leonidkurepin@yandex.ru>
2020-02-29 21:21:42 +0400

    Customize home page and create page drafts

---
+++ BooksBlogRazor/BooksBlogRazor/Pages/BookReviews/ShortBookReviewsEntitiesList.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BooksBlogRazor.Pages.BookReviews
+{
+    public class ShortBookReviewsEntitiesListModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
